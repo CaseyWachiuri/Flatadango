3afee71d634ff5bae65b130eefe25bc5f1b91970
Casey Waweru <casey.waweru@gmail.com>
2025-10-01 22:32:57 +0300

    Adding functionality to update movie poster and details when the user clicks the movie list

--- script.js
+++ script.js
@@ -4,8 +4,18 @@ const init = () => {
 
 document.addEventListener("DOMContentLoaded", init);
 
+// Declaring selected Elements to variable names
+const movieList = document.querySelector("ul#films");
+
+const moviePoster = document.querySelector("img#poster");
+
+const movieTitle = document.querySelector("h2#title");
+const movieRuntime = document.querySelector("p#runtime");
+const movieDescription = document.querySelector("p#description");
+const movieShowtime = document.querySelector("p#showtime");
+const movieTickets = document.querySelector("p#tickets");
+
 function displayMovie() {
-  const movieList = document.querySelector("ul#films");
 
   fetch("http://localhost:3000/films")
     .then(r => r.json())
@@ -14,14 +24,31 @@ function displayMovie() {
 	const li = document.createElement("li");
 	li.innerText = movie.title;
 	movieList.append(li);
-	li.addEventListener("click", () => {});
+	li.addEventListener("click", () => {getMovie(movie)});
       });
     });
 }
 
-function getMovie() {
-  // Check number of tickets remaining
-  if (tickets == 0) {
-    // Update the UI to say tickets are no longer available
-  }
+function getMovie(movie) {
+  const availableTickets = movie.capacity - movie.tickets_sold
+
+  moviePoster.src = movie.poster;
+  moviePoster.alt = movie.title;
+
+  movieTitle.innerText = movie.title;
+  movieRuntime.innerText = `${movie.runtime} minutes`;
+  movieDescription.innerText = movie.description;
+  movieShowtime.innerText = movie.showtime;
+  movieTickets.innerText = `${availableTickets} tickets remaining`;
 }
+
+/*
+"id": "1",
+"title": "The Giant Gila Monster",
+"runtime": "108",
+"capacity": 30,
+"showtime": "04:00PM",
+"tickets_sold": 27,
+"description": "A giant lizard terrorizes a rural Texas community and a heroic teenager attempts to destroy the creature.",
+"poster": "https://www.gstatic.com/tv/thumb/v22vodart/2157/p2157_v_v8_ab.jpg"
+*/
