1a4c33afe88a06251e94bd227ad847882eafa566
Casey Waweru <casey.waweru@gmail.com>
2025-10-01 22:08:07 +0300

    Adding Functionality to update the movie list

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 const init = () => {
-  getMovie();
+  displayMovie();
 }
 
 document.addEventListener("DOMContentLoaded", init);
 
-function getMovie() {
+function displayMovie() {
+  const movieList = document.querySelector("ul#films");
 
   fetch("http://localhost:3000/films")
     .then(r => r.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+      data.forEach(movie => {
+	const li = document.createElement("li");
+	li.innerText = movie.title;
+	movieList.append(li);
+	li.addEventListener("click", () => {});
+      });
+    });
 }
 
-function numberOfTickets() {
+function getMovie() {
   // Check number of tickets remaining
   if (tickets == 0) {
     // Update the UI to say tickets are no longer available
